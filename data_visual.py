c14b77280af442732e5531b4c549e429ab2834cb
Geoforger <brayshaw.george@gmail.com>
2021-06-03 21:29:58 +0100

    Completed heatmap creator

--- data_visual.py
+++ data_visual.py
@@ -2,26 +2,68 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Create array to contain the intensities of
+# Create array to contain the intensities of events
 intensity = np.zeros([240, 180])
+
+# Path to dataset
+# Change folder path for each dataset?
 FOLDER_NAME = "ntac_2.5_6texture_20trial_slide_test_06031504"
-PATH = "/Documents/PhD/Spiking Nets Project/SpikingNetsTexture/datasets/TacTip_NM/" + FOLDER_NAME
+PATH = "/home/farscope2/Documents/PhD/Spiking Nets Project/SpikingNetsTexture/datasets/TacTip_NM/" + FOLDER_NAME
 
-with(open("SingleTap_run_0_orientation_1.pickle", "rb")) as openfile:
-    try:
-        orig_array = pickle.load(openfile)
-    except EOFError:
-        print(EOFError)
+# Number of textures tested & number of trials per texture
+textures = 6
+trials = 20
 
-for z in range(len(orig_array)):
-    for y in range(len(orig_array[z])):
-        intensity[z, y] = len(orig_array[z, y])
+# Maximum intensity of events seen across entire dataset
+# Used to scale the heatmaps
+max_intensity = 0
 
+# Open each file and find the highest intensity in order to find an appropriate vmax
+for t in range(trials):
+    for s in range(textures):
+        # Open each file in dataset
+        FILENAME = PATH + "/SingleTap_run_" + \
+            str(t) + "_orientation_" + str(s) + ".pickle"
 
-# Plot heatmap of events
-plt.imshow(intensity, cmap='hot', interpolation='nearest', vmin=0, vmax=22)
-plt.ylabel('Y Pixels')
-plt.xlabel('X Pixels')
-plt.colorbar()
-plt.savefig("SingleTap_run_0_orientation_1.png")
-plt.show()
+        with(open(FILENAME, "rb")) as openfile:
+            try:
+                orig_array = pickle.load(openfile)
+            except EOFError:
+                print(EOFError)
+
+        for z in range(len(orig_array)):
+            for y in range(len(orig_array[z])):
+                intensity[z, y] = len(orig_array[z, y])
+
+        file_intensity = np.max(intensity)
+
+        if file_intensity > max_intensity:
+            max_intensity = file_intensity
+
+for xx in range(trials):
+    for yy in range(textures):
+        # Open each file individually
+        FILENAME = PATH + "/SingleTap_run_" + \
+            str(xx) + "_orientation_" + str(yy) + ".pickle"
+
+        # Create array of intensities for heatmap
+        with(open(FILENAME, "rb")) as openfile:
+            try:
+                orig_array = pickle.load(openfile)
+            except EOFError:
+                print(EOFError)
+
+        for u in range(len(orig_array)):
+            for i in range(len(orig_array[u])):
+                intensity[u, i] = len(orig_array[u, i])
+
+        # Plot heatmap of events
+        plt.imshow(intensity, cmap='hot', interpolation='nearest',
+                   vmin=0, vmax=max_intensity)
+        plt.ylabel('Y Pixels')
+        plt.xlabel('X Pixels')
+        plt.colorbar()
+        plt.savefig("graphs/SingleTap_run_" +
+                    str(xx) + "_orientation_" + str(yy) + ".pickle" + ".png")
+        plt.clf()  # Clear figure post save
+        # plt.show()
